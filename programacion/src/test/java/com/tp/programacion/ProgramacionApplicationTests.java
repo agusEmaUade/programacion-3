38652c71fc9e296b5a2321cd96f2043c215688bd
agusEmaUade <agyanez@uade.edu.ar>
2025-02-16 14:52:49 -0300

    feat: init config

---
+++ programacion/src/test/java/com/tp/programacion/ProgramacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.programacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
